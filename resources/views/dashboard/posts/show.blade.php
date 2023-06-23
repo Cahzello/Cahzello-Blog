85564cb364070392a6b8de07fe66415a805715af
Cahzello <rizkyrasya.m@gmail.com>
2023-06-23 15:09:41 +0700

    add edit and delete feature

--- resources/views/dashboard/posts/show.blade.php
+++ resources/views/dashboard/posts/show.blade.php
@@ -5,14 +5,22 @@
     <div class="row my-3">
         <div class="col-lg-8">
             <h1 class="mb-3">{{ $post->title }}</h1>
-            <a href="/dashboard/posts" class="btn btn-success mb-3"><span data-feather="arrow-left"></span> Back to my posts</a>
-            <a href="" class="btn btn-warning mb-3"><span data-feather="edit"></span> Edit</a>
-            <a href="" class="btn btn-danger mb-3"><span data-feather="x-circle"></span> Delete</a>
+            <div class="mb-3">
+                <a href="/dashboard/posts" class="btn btn-success"><span data-feather="arrow-left"></span> Back to my posts</a>
+                <a href="/dashboard/posts/{{ $post->slug }}/edit" class="btn btn-warning"><span data-feather="edit"></span> Edit</a>
+                <form action="/dashboard/posts/{{ $post->slug }}" method="POST" class="d-inline">
+                    @method('delete')
+                    @csrf
+                    <button class="btn btn-danger" onclick="return confirm('Are you sure want to delete this post?')">
+                        <span data-feather="x-circle"></span> Delete
+                    </button>
+                </form>
+            </div>
             <img src="https://picsum.photos/id/{{ $post->id }}/1200/400" alt="{{ $post->category->name }}" class="img-fluid">
             <article class="my-3 fs-5">
                 {!! $post->body !!} 
             </article>
-            <a href="/dashboard/posts" class="d-block mt-3">Back to my posts</a>
+            <a href="/dashboard/posts" class="btn btn-success"><span data-feather="arrow-left"></span> Back to my posts</a>
         </div>
     </div>
 </div>
