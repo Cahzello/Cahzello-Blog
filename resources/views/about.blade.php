8efd31e75fae30147e56c629a1b96a685a659725
Cahzello <rizkyrasya.m@gmail.com>
2023-06-25 16:06:58 +0700

    fix UI

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -11,7 +11,6 @@
             At its core, our Blogger website serves as a dynamic platform for bloggers of all backgrounds, interests, and expertise to express themselves and engage with readers from around the globe. Whether you're an aspiring writer, a passionate photographer, a food enthusiast, or a tech aficionado, you'll find a welcoming space here to share your thoughts, experiences, and unique perspectives.    
         </p>
         <p class="lead">
-            <a href="#" class="btn btn-lg btn-secondary fw-bold border-white bg-white">Learn more</a>
         </p>
 
     </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
             <p class="lead">
                 Our website serves the purposes of content sharing, community building, skill enhancement, inspiration and exploration, networking and collaboration, and personal and professional growth for bloggers.</p>                
             <p class="lead">
-                <a href="#" class="btn btn-lg btn-secondary fw-bold border-white bg-white">Learn more</a>
+                <a href="/login" class="btn btn-lg btn-secondary fw-bold border-white bg-dark">Learn more</a>
             </p>
         </div>
     </main>
