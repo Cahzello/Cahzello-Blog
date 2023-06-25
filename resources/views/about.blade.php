bfed427454c342cbe04a89ff1ae4d5ac8a67863b
Cahzello <rizkyrasya.m@gmail.com>
2023-06-25 16:05:40 +0700

    UI fix

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,15 +1,19 @@
 @extends('layouts.main')
 
 @section('container')
-<div class="container">
-    <div class="container">
-        <h1>Halaman About</h1>
-        <h3> {{$name}} </h3>
-        <h3> {{$email}} </h3>
-    </div>
-    <div class="container">
-        <img src="img/{{$image}}" alt="{{$name}}" title="{{$name}}" width="300" class="img-thumbnail ">    
-    </div>
+<main class="px-3">
+    <div class="text-center">
+        <h1>About</h1>
+        <p class="lead">
+            Welcome to our Blogger website! We are thrilled to have you here and introduce you to a platform designed for sharing and connecting with a global audience. As you navigate through our website's first home page, you'll discover a world of creative possibilities and insightful content.
+        </p>
+        <p class="lead">
+            At its core, our Blogger website serves as a dynamic platform for bloggers of all backgrounds, interests, and expertise to express themselves and engage with readers from around the globe. Whether you're an aspiring writer, a passionate photographer, a food enthusiast, or a tech aficionado, you'll find a welcoming space here to share your thoughts, experiences, and unique perspectives.    
+        </p>
+        <p class="lead">
+            <a href="#" class="btn btn-lg btn-secondary fw-bold border-white bg-white">Learn more</a>
+        </p>
 
-</div>
+    </div>
+</main>
 @endsection 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,14 @@
 @extends('layouts.main')
 
 @section('container')
-    <div class="text-center mb-3">
-        <h1>Home</h1>
-    </div>
-
-    
-
+    <main class="px-3">
+        <div class="text-center">
+            <h1>Welcome.</h1>
+            <p class="lead">
+                Our website serves the purposes of content sharing, community building, skill enhancement, inspiration and exploration, networking and collaboration, and personal and professional growth for bloggers.</p>                
+            <p class="lead">
+                <a href="#" class="btn btn-lg btn-secondary fw-bold border-white bg-white">Learn more</a>
+            </p>
+        </div>
+    </main>
 @endsection
