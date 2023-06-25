431333b831e372e1104ad46057548852fa8ca29e
Cahzello <rizkyrasya.m@gmail.com>
2023-06-25 18:17:10 +0700

    fix footer

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -10,13 +10,35 @@
     <link rel="stylesheet" href="/css/style.css">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
     {{-- Bootstrap Icons --}}
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 
-    <title>Cahzello | {{$title}}</title>
+    <title>Cahzello | {{ $title }}</title>
 
+    <style>
+        html {
+            position: relative;
+            min-height: 100%;
+        }
+
+        body {
+            margin-bottom: 160px;
+            /* Margin bottom by footer height */
+        }
+
+        .footer {
+            position: absolute;
+            bottom: 0;
+            width: 100%;
+            height: 150px;
+            /* Set the fixed height of the footer here */
+            line-height: auto;
+            /* Vertically center the text there */
+        }
+    </style>
 </head>
 
 <body>
